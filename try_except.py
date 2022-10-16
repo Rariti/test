796aaeb482ce962c11f4be4e607acc93d835ce69
Rariti <30593773+Rariti@users.noreply.github.com>
2022-10-16 15:32:31 +0300

    Delete try_except.py

--- try_except.py
+++
@@ -1,8 +0,0 @@
-try:
-    text = input('Введите что нибудь --->')
-except EOFError:
-    print('Ну зачем вы сделали EOFError')
-except KeyboardInterrupt:
-    print('Нузачем мне KeyboardInterrupt')
-else:
-    print('Все впорядке {0}'.format(text))
